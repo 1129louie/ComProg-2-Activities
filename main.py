b7129bc206d9a7496d9c00bb077b748b10652a97
1129louie <chabitsheena29@gmail.com>
2026-02-10 14:09:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+fullname = "Sheena Louie M. Chabit"   
+
+age = 19                            
+
+height = 1.50                       
+
+currently_working = False            
+
+print("Fullname:", fullname, "| Type:", type(fullname))
+print("Age:", age, "| Type:", type(age))
+print("Height in meters:", height, "| Type:", type(height))
+print("Currently working?:", currently_working, "| Type:", type(currently_working))
